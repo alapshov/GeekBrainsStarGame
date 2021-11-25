e282c509775764f5912297baef1839c09aef74af
Алексей <avlapshov@dasreda.ru>
2021-11-25 10:16:05 +0300

    Task 2.

--- core/src/ru/geekbrains/stargame/screen/impl/MenuScreen.java
+++ core/src/ru/geekbrains/stargame/screen/impl/MenuScreen.java
@@ -4,16 +4,16 @@ import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.math.Vector2;
 
-import ru.geekbrains.stargame.helpers.RenderHelper;
 import ru.geekbrains.stargame.screen.BaseScreen;
 
 public class MenuScreen extends BaseScreen {
 
+    private final float V_LEN = 5f;
     private Texture img;
     private Texture background;
     private Vector2 touch;
+    private Vector2 pos;
     private Vector2 v;
-    private Vector2 v2;
 
 
     @Override
@@ -21,20 +21,24 @@ public class MenuScreen extends BaseScreen {
         super.show();
         img = new Texture("badlogic.jpg");
         background = new Texture("Space.png");
+        pos = new Vector2();
         touch = new Vector2();
-        v = new Vector2(0, 0);
-        v2 = new Vector2(0, 0);
+        v = new Vector2();
     }
 
     @Override
     public void render(float delta) {
         super.render(delta);
-        v = RenderHelper.getDirectionVector(v2, touch, v);
-        v2.add(v);
         batch.begin();
-        batch.draw(img, v2.x, v2.y);
+        batch.draw(img, pos.x, pos.y);
         batch.end();
-        System.out.println(v.x +"  "+ v.y);
+
+        if(touch.dst(pos) > V_LEN) {
+            pos.add(v);
+        } else {
+            pos.set(touch);
+        }
+
 
     }
 
@@ -48,6 +52,7 @@ public class MenuScreen extends BaseScreen {
     @Override
     public boolean touchDown(int screenX, int screenY, int pointer, int button) {
         touch.set(screenX, Gdx.graphics.getHeight() - screenY);
+        v.set(touch.cpy().sub(pos)).setLength(V_LEN);
         return super.touchDown(screenX, screenY, pointer, button);
     }
 }
