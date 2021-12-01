9d57f0707fc17652b3674775bb6e5f2c1193622c
Алексей <avlapshov@dasreda.ru>
2021-12-01 22:30:12 +0300

    Task_4

--- core/src/ru/geekbrains/stargame/screen/impl/GameScreen.java
+++ core/src/ru/geekbrains/stargame/screen/impl/GameScreen.java
@@ -1,22 +1,36 @@
 package ru.geekbrains.stargame.screen.impl;
 
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
 import com.badlogic.gdx.math.Vector2;
 
 import ru.geekbrains.stargame.math.Rect;
 import ru.geekbrains.stargame.screen.BaseScreen;
 import ru.geekbrains.stargame.sprite.impl.Background;
+import ru.geekbrains.stargame.sprite.impl.Star;
 
 public class GameScreen extends BaseScreen {
 
+    private static final int STAR_COUNT = 64;
+
     private Texture bg;
     private Background background;
+
+    private TextureAtlas atlas;
+    private Star[] stars;
 
     @Override
     public void show() {
         super.show();
         bg = new Texture("textures/bg.png");
         background = new Background(bg);
+
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
+
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(atlas);
+        }
     }
 
     @Override
@@ -30,6 +44,9 @@ public class GameScreen extends BaseScreen {
     public void resize(Rect worldBounds) {
         super.resize(worldBounds);
         background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
     }
 
     @Override
@@ -49,12 +66,17 @@ public class GameScreen extends BaseScreen {
     }
 
     private void update(float delta){
-
+        for (Star star : stars) {
+            star.update(delta);
+        }
     }
 
     private void draw(){
         batch.begin();
         background.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
         batch.end();
     }
 }
