4882aa4accb1af8bdf88422210c2c0222302adb4
Алексей <avlapshov@dasreda.ru>
2021-12-05 15:47:53 +0300

    Урок 5

---
+++ core/src/ru/gb/pool/impl/BulletPool.java
@@ -0,0 +1,11 @@
+package ru.gb.pool.impl;
+
+import ru.gb.pool.SpritesPool;
+import ru.gb.sprite.impl.Bullet;
+
+public class BulletPool extends SpritesPool<Bullet> {
+    @Override
+    protected Bullet newObject() {
+        return new Bullet();
+    }
+}
---
+++ core/src/ru/gb/screen/impl/GameScreen.java
@@ -0,0 +1,115 @@
+package ru.gb.screen.impl;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureAtlas;
+import com.badlogic.gdx.math.Vector2;
+
+import ru.gb.math.Rect;
+import ru.gb.pool.impl.BulletPool;
+import ru.gb.screen.BaseScreen;
+import ru.gb.sprite.impl.Background;
+import ru.gb.sprite.impl.MainShip;
+import ru.gb.sprite.impl.Star;
+
+public class GameScreen extends BaseScreen {
+
+    private static final int STAR_COUNT = 64;
+
+    private Texture bg;
+    private Background background;
+
+    private BulletPool bulletPool;
+
+    private TextureAtlas atlas;
+    private Star[] stars;
+    private MainShip mainShip;
+
+    @Override
+    public void show() {
+        super.show();
+        bg = new Texture("textures/bg.png");
+        background = new Background(bg);
+        atlas = new TextureAtlas("textures/mainAtlas.tpack");
+
+        bulletPool = new BulletPool();
+
+        stars = new Star[STAR_COUNT];
+        for (int i = 0; i < stars.length; i++) {
+            stars[i] = new Star(atlas);
+        }
+        mainShip = new MainShip(atlas, bulletPool);
+    }
+
+    @Override
+    public void render(float delta) {
+        super.render(delta);
+        update(delta);
+        freeAllDestroyed();
+        draw();
+    }
+
+    @Override
+    public void resize(Rect worldBounds) {
+        super.resize(worldBounds);
+        background.resize(worldBounds);
+        for (Star star : stars) {
+            star.resize(worldBounds);
+        }
+        mainShip.resize(worldBounds);
+    }
+
+    @Override
+    public void dispose() {
+        super.dispose();
+        bg.dispose();
+        atlas.dispose();
+        bulletPool.dispose();
+    }
+
+    @Override
+    public boolean touchDown(Vector2 touch, int pointer, int button) {
+        mainShip.touchDown(touch, pointer, button);
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(Vector2 touch, int pointer, int button) {
+        mainShip.touchUp(touch, pointer, button);
+        return false;
+    }
+
+    @Override
+    public boolean keyDown(int keycode) {
+        mainShip.keyDown(keycode);
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        mainShip.keyUp(keycode);
+        return false;
+    }
+
+    private void update(float delta) {
+        for (Star star : stars) {
+            star.update(delta);
+        }
+        mainShip.update(delta);
+        bulletPool.updateActiveSprites(delta);
+    }
+
+    private void freeAllDestroyed() {
+        bulletPool.freeAllDestroyed();
+    }
+
+    private void draw() {
+        batch.begin();
+        background.draw(batch);
+        for (Star star : stars) {
+            star.draw(batch);
+        }
+        mainShip.draw(batch);
+        bulletPool.drawActiveSprites(batch);
+        batch.end();
+    }
+}
